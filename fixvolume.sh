041b57b9f57feafa920a84136b7fc87ffd487e44
KAMiKAZOW <kamikazow@web.de>
2016-11-28 06:05:46 +0100

    PCM nicht vergessen

--- fixvolume.sh
+++ fixvolume.sh
@@ -6,8 +6,12 @@
 
 # Headphone und Speaker dürfen nicht stumm sein:
 amixer --device=hw --card=1 sset Headphone unmute
-amixer --device=hw --card=1 sset Headphone 100%
 amixer --device=hw --card=1 sset Speaker unmute
+
+# Lautstärke für Headphone und PCM sollten auf 100% sein,
+# damit nur noch Master anzupassen ist.
+amixer --device=hw --card=1 sset Headphone 100%
+amixer --device=hw --card=1 sset PCM 100%
 
 # Über die Speaker soll aber kein Ton kommen:
 amixer --device=hw --card=1 sset Speaker 0%
